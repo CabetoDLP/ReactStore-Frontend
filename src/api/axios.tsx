b74bbffb4bd2d3ec8fd7c292f290638d5064b677
cabetoDLP <cabetodp@gmail.com>
2025-02-28 20:56:23 -0500

    Axios url changed

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000'; // https://react-store.azurewebsites.net http://localhost:5000 
+const BASE_URL = 'https://react-store.azurewebsites.net'; //  http://localhost:5000 
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
